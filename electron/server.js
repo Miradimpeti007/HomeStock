285482147acdf2eec4d6e4c19e4a2e33fa86d439
miradi mpeti eboma <miradimpeti007@gmail.com>
2026-03-10 01:05:50 +0100

    Revert "mise à jour de la classe server"
    This reverts commit 21e37d9f786349918170ff6398f2291b9f8b7ff7.

--- electron/server.js
+++ electron/server.js
@@ -1,6 +1,5 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const { registerAllIpc } = require('./ipc/index');
 
 let mainWindow;
 
@@ -18,14 +17,11 @@ function createWindow() {
 
     mainWindow.maximize();
     mainWindow.show();
-    mainWindow.webContents.openDevTools();
 
-    mainWindow.loadFile(path.join(__dirname, '../test/dashboard.html'));
+    mainWindow.loadURL('http://localhost:3000');
 }
 
 app.whenReady().then(() => {
-
-    registerAllIpc();
     createWindow();
 
     app.on('activate', () => {
